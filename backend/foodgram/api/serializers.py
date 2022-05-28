1d11c2588c1b64748a5fc8a46fc181dbb4a93446
pro100cemuk <pro100cemuk@gmail.com>
2022-05-29 00:46:12 +0300

    changed serializers

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,8 +1,7 @@
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
-from api.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
-                        ShoppingCart, Tag)
+from api.models import Ingredient, IngredientRecipe, Recipe, Tag
 from users.serializers import CustomUserSerializer
 
 
