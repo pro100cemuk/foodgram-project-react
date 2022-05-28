5780877405efab1c3302d02f8b8a075722cc31fd
pro100cemuk <pro100cemuk@gmail.com>
2022-05-29 02:12:12 +0300

    fixed bug

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -156,18 +156,18 @@ class RecipesWriteSerializer(serializers.ModelSerializer):
         return recipe
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.image = validated_data.get('image', instance.image)
-        instance.text = validated_data.get('text', instance.text)
-        instance.cooking_time = validated_data.get(
+        instance.name = validated_data.pop('name', instance.name)
+        instance.image = validated_data.pop('image', instance.image)
+        instance.text = validated_data.pop('text', instance.text)
+        instance.cooking_time = validated_data.pop(
             'cooking_time',
             instance.cooking_time
         )
         instance.ingredients.clear()
-        ingredients = validated_data.get('ingredients')
+        ingredients = validated_data.pop('ingredients')
         self.create_ingredients(ingredients, instance)
         instance.tags.clear()
-        tags = validated_data.get('tags')
+        tags = validated_data.pop('tags')
         self.create_tags(tags, instance)
         return super().update(instance, validated_data)
 
